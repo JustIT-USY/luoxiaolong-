1ca9d460a52f4576823bd1ac5787f108e3b49ac4
luoxiaolong <43778433+luoxiaolong9@users.noreply.github.com>
2018-10-14 20:34:28 +0800

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,17 @@
+package first1;
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("出发地：昆明，终点站：上海");
+		System.out.println("车次：T382");
+		System.out.println("时间：2014年08月12日18：49开");
+		System.out.println("座位：10车095号");
+		System.out.println("车辆信息：新空调特快硬座");
+		System.out.println("乘车人：马琦");
+		System.out.println("身份证号：5304231995****0616");
+
+	}
+
+}
